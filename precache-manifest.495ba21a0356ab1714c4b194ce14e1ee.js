0b199ec903137f5af1f89a617271808e3c82fd22
VTVIVEK <vivek78643@gmail.com>
2020-07-20 11:45:39 +0530

    Updates

---
+++ precache-manifest.495ba21a0356ab1714c4b194ce14e1ee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57168b8cb771216596114771043723ad",
+    "url": "/wellness/index.html"
+  },
+  {
+    "revision": "16b6036e71126be5204c",
+    "url": "/wellness/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "111881b59dedaa4b7f29",
+    "url": "/wellness/static/css/main.4fdebe9b.chunk.css"
+  },
+  {
+    "revision": "16b6036e71126be5204c",
+    "url": "/wellness/static/js/2.5695fff4.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/wellness/static/js/2.5695fff4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "111881b59dedaa4b7f29",
+    "url": "/wellness/static/js/main.ea2438c4.chunk.js"
+  },
+  {
+    "revision": "92af40ba2dd12e479f87",
+    "url": "/wellness/static/js/runtime-main.d33e48bd.js"
+  },
+  {
+    "revision": "8c917a68f048fad74fd03db2db58c3d0",
+    "url": "/wellness/static/media/home.8c917a68.mp4"
+  }
+]);
